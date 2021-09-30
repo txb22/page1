956300951d6524e1ed9ef946cf208b17fbc1a51d
txb22 <tranxuanbacit@gmail.com>
2021-09-30 14:41:32 +0700

    first commit

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,22 +1,31 @@
-var tabLinks = document.querySelectorAll(".tablinks");
-var tabContent = document.querySelectorAll(".tabcontent");
+// var tabLinks = document.querySelectorAll(".tablinks");
+// var tabContent = document.querySelectorAll(".tabcontent");
 
-tabLinks.forEach(function (el) {
-    el.addEventListener("click", openTabs);
-});
-function openTabs(el) {
-    var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
-    var electronic = btn.dataset.electronic; // lấy giá trị trong data-electronic
+// tabLinks.forEach(function (el) {
+//     el.addEventListener("click", openTabs);
+// });
+// function openTabs(el) {
+//     var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
+//     var electronic = btn.dataset.electronic; // lấy giá trị trong data-electronic
 
-    tabContent.forEach(function (el) {
-        el.classList.remove("active");
-    });
+//     tabContent.forEach(function (el) {
+//         el.classList.remove("active");
+//     });
 
-    tabLinks.forEach(function (el) {
-        el.classList.remove("active");
-    });
+//     tabLinks.forEach(function (el) {
+//         el.classList.remove("active");
+//     });
 
-    document.querySelector("#" + electronic).classList.add("active");
+//     document.querySelector("#" + electronic).classList.add("active");
 
-    btn.classList.add("active");
-}
+//     btn.classList.add("active");
+// }
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+      var tabTrigger = new bootstrap.Tab(triggerEl)
+
+      triggerEl.addEventListener('click', function (event) {
+            event.preventDefault()
+            tabTrigger.show()
+      })
+})
